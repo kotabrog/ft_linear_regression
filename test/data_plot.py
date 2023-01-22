7dcf7e9cf64a1efa32cb5a4b3476ef4c1c2f2602
kotabrog <suzuki.kota0331@gmail.com>
2023-01-22 19:46:45 +0900

    complete algorithm

--- test/data_plot.py
+++ test/data_plot.py
@@ -22,22 +22,29 @@ if __name__ == '__main__':
     plt.ylabel("price")
     ax.scatter(x, y, s=100, c='r', alpha=0.3)
 
-    model = linear_regression.LinearRegression(learning_rate=1e-7)
+    model = linear_regression.LinearRegression(learning_rate=1e-1)
     # model.load('data.pickle')
 
-    model.train(x, y, epoch=1)
+    # model.train(x, y, epoch=1, verbose=True)
+
+    # x_l = np.linspace(x_min, x_max, 100)
+    # plt.plot(x_l, model.predict(x_l), 'b:')
+
+    model.train(x, y, epoch=1000, verbose=True)
 
     x_l = np.linspace(x_min, x_max, 100)
-    plt.plot(x_l, model.predict(x_l), 'b:')
+    plt.plot(x_l, model.predict(x_l), 'r:', label="my model")
 
-    model.train(x, y, epoch=10)
+    # model.train(x, y, epoch=100)
+
+    # x_l = np.linspace(x_min, x_max, 100)
+    # plt.plot(x_l, model.predict(x_l), 'g:')
+
+    k = np.polyfit(x, y, 1)
+    print(k)
 
     x_l = np.linspace(x_min, x_max, 100)
-    plt.plot(x_l, model.predict(x_l), 'r:')
+    plt.plot(x_l, k[1] + k[0] * x_l, 'g:', label="polyfit")
 
-    model.train(x, y, epoch=100)
-
-    x_l = np.linspace(x_min, x_max, 100)
-    plt.plot(x_l, model.predict(x_l), 'g:')
-
+    plt.legend()
     plt.show()
--- test/linear_regression.py
+++ test/linear_regression.py
@@ -13,17 +13,28 @@ class LinearRegression:
     def _train_one(self, x, y):
         # xとyのサイズのerror処理.
         # x, yがサイズ0のときのerror処理
-        # self.theta0, self.theta1 = (self.theta0 - self.learning_rate * (sum([self.predict(mileage) for mileage in x]) - sum(y)) / len(x),
-        #                             self.theta1 - self.learning_rate * sum([(self.predict(mileage) - price) * mileage for mileage, price in zip(x, y)]) / len(x))
-        self.theta0, self.theta1 = (self.learning_rate * (sum([self.predict(mileage) for mileage in x]) - sum(y)) / len(x),
-                                    self.learning_rate * sum([(self.predict(mileage) - price) * mileage for mileage, price in zip(x, y)]) / len(x))
+        self.theta0, self.theta1 = (self.theta0 - self.learning_rate * (sum([self.predict(mileage) for mileage in x]) - sum(y)) / len(x),
+                                    self.theta1 - self.learning_rate * sum([(self.predict(mileage) - price) * mileage for mileage, price in zip(x, y)]) / len(x))
+        # self.theta0, self.theta1 = (self.learning_rate * (sum([self.predict(mileage) for mileage in x]) - sum(y)) / len(x),
+        #                             self.learning_rate * sum([(self.predict(mileage) - price) * mileage for mileage, price in zip(x, y)]) / len(x))
 
     def train(self, x, y, epoch=1, verbose=False):
         # x, y, epochのerror処理
+        max_x = max(x)
+        max_y = max(y)
+        max_value = max(max_x, max_y)
+
+        norm_x = x / max_value
+        norm_y = y / max_value
+
+        self.norm = max_value
+
         for i in range(epoch):
-            self._train_one(x, y)
+            self._train_one(norm_x, norm_y)
             if verbose:
                 print("{}/{}: theta0:".format(i, epoch), self.theta0, "theta1:", self.theta1)
+
+        self.theta0 *= max_value
 
 
     def dump(self, file='data.pickle'):
